dd774715ef38e75f7e9386e2f84f9f25fddbbcee
nce27 <nc3elliott@gmail.com>
2023-05-18 14:13:42 +0100

    Added a fail message if returned an empty text

--- lib/grammar_stats.rb
+++ lib/grammar_stats.rb
@@ -6,17 +6,16 @@ class GrammarStats
   def check(text) # text is a string
     # Returns true or false depending on whether the text begins with a capital
     # letter and ends with a sentence-ending punctuation mark.
+
+    fail "Cannot check an empty string" if text.empty? #fails if given an empty string 
+
     first_char = text[0] #first letter of the text"
     last_char = text[-1]
 
-    ends_with_punctuation = last_char.match? (/[.!?]/)
+    ends_with_punctuation = last_char.match? (/[.!?]/) 
     starts_with_capital = first_char.match? (/[A-Z]/)
 
     starts_with_capital && ends_with_punctuation
-
-    # valid_punctuation = (".", "!", "?")
-    # last_char.include? valid_punctuation
-
 
   end
 
