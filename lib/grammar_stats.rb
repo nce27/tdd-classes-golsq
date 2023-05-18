4e6fcd13abe7c0aa7967414f19573787defb7285
nce27 <nc3elliott@gmail.com>
2023-05-18 14:48:39 +0100

    Updated lib file with condition statement to incriment pass_count and toal_count variables for each test. Also added calculation for percentage_good method to calculate the percentage.

--- lib/grammar_stats.rb
+++ lib/grammar_stats.rb
@@ -1,6 +1,8 @@
 class GrammarStats
   def initialize
     @check = true
+    @pass_count = 0
+    @total_count = 0
   end
 
   def check(text) # text is a string
@@ -15,12 +17,27 @@ class GrammarStats
     ends_with_punctuation = last_char.match? (/[.!?]/) 
     starts_with_capital = first_char.match? (/[A-Z]/)
 
-    starts_with_capital && ends_with_punctuation
+    result = starts_with_capital && ends_with_punctuation
 
+    if result
+      @pass_count += 1
+    end
+      @total_count += 1
+      result
   end
 
   def percentage_good
     # Returns as an integer the percentage of texts checked so far that passed
     # the check defined in the `check` method. The number 55 represents 55%.
+
+    # counter of tests passing
+    
+    # counter of total tests
+
+    if @total_count == 0
+      return 0.0
+    else
+      return (@pass_count.to_f / @total_count.to_f) * 100.0
+    end
   end
 end
