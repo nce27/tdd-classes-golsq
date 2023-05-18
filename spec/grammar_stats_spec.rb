e00417a86308fc9e5ffff1d43c63de3d3d0f3f04
nce27 <nc3elliott@gmail.com>
2023-05-18 14:15:24 +0100

    Updated spec test file with context for readability and an appropriate erroneous test when thrown an empty string.

--- spec/grammar_stats_spec.rb
+++ spec/grammar_stats_spec.rb
@@ -2,9 +2,17 @@ require 'grammar_stats'
 
 RSpec.describe GrammarStats do
   describe "#check" do
-    it "returns true if it is a valid sentence." do
-      stats = GrammarStats.new
-      expect(stats.check("Hello world.")).to eq true
-    end
+
+    context "when given an empty string" do
+      it "fails" do
+        grammar = GrammarStats.new
+        expect { grammar.check("") }.to raise_error "Cannot check an empty string"
+      end
+    end  
+
+      it "returns true if it is a valid sentence." do
+        grammar = GrammarStats.new
+        expect(grammar.check("Hello world.")).to eq true
+      end
   end
 end
